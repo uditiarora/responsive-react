54ef2d1fa4b89b2b1ee33957ea5eb5b75cb17f8d
Uditi <uditiarora@gmail.com>
2020-04-25 12:34:51 +0530

    Updates

---
+++ precache-manifest.8f7478328bd1af50194f6e030f520155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78777fb9cc3c7f3f5b28698db9d4ce3a",
+    "url": "/responsive-react/index.html"
+  },
+  {
+    "revision": "2122d9279ae05c50fc4c",
+    "url": "/responsive-react/static/css/main.7f274844.chunk.css"
+  },
+  {
+    "revision": "d35fb712d03b59b848cc",
+    "url": "/responsive-react/static/js/2.f4d5f6e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/responsive-react/static/js/2.f4d5f6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2122d9279ae05c50fc4c",
+    "url": "/responsive-react/static/js/main.860f0b17.chunk.js"
+  },
+  {
+    "revision": "5182865acb6112245357",
+    "url": "/responsive-react/static/js/runtime-main.4e272db7.js"
+  }
+]);
